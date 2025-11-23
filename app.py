a009ecee20bec10ee440331994257863b140b7f7
Seerious <mkarowich13@gmail.com>
2025-11-23 13:40:05 -0500

    Remove app from debug mode

--- app.py
+++ app.py
@@ -46,5 +46,5 @@ def random_fact():
     return jsonify({"source": "local", "fact": random.choice(local)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
 
